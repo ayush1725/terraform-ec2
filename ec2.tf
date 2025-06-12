44f4c105584d3594998479efc888b780f8fd8067
Ayush <ayushdwibedy123@gmail.com>
2025-06-13 00:53:16 +0530

    Added EC2 instance with EBS volume and security group

--- ec2.tf
+++ ec2.tf
@@ -48,6 +48,7 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 #ec2 instance
+
 resource "aws_instance" "my_instance" {
   count = 2 # meta argument to create multiple instances
   /*
@@ -56,19 +57,22 @@ resource "aws_instance" "my_instance" {
     "TWS-junoon-automate-medium" = "t2.medium"
   }) # meta argument to create multiple instances with different instance types
   */
+
+  depends_on = [aws_security_group.my_security_group, aws_key_pair.key_pair]
+
   key_name        = aws_key_pair.key_pair.key_name
   security_groups = [aws_security_group.my_security_group.name]
-  instance_type   = var.ec2_instance_type    # "t2.micro" or any other instance type you want to use
-                   # each.value # for_each.value if using for_each
-  ami             = var.ec2_ami_id           # Ubuntu Server 24.04 LTS (HVM)
-  user_data       = file("install_nginx.sh") # Script to install Nginx and Flask app
+  instance_type   = var.ec2_instance_type # "t2.micro" or any other instance type you want to use
+  # each.value # for_each.value if using for_each
+  ami       = var.ec2_ami_id           # Ubuntu Server 24.04 LTS (HVM)
+  user_data = file("install_nginx.sh") # Script to install Nginx and Flask app
 
   root_block_device {
-    volume_size = var.ec2_root_storage_size # Size in GB
-    volume_type = "gp3"                     # General Purpose SSD
+    volume_size = var.env == "prd" ? 20 : var.ec2_default_root_storage_size # Size in GB
+    volume_type = "gp3"                                                     # General Purpose SSD
   }
   tags = {
     Name = "TF-EC2-automate"
-            # each.key # for_each.key if using for_each
+    # each.key # for_each.key if using for_each
   }
 }
--- variables.tf
+++ variables.tf
@@ -3,12 +3,17 @@ variable "ec2_instance_type" {
   type    = string
 }
 
-variable "ec2_root_storage_size" {
-  default = 13
+variable "ec2_default_root_storage_size" {
+  default = 10 # Size in GB
   type    = number
 }
 
 variable "ec2_ami_id" {
   default = "ami-02521d90e7410d9f0"
+  type    = string
+}
+
+variable "env" {
+  default = "dev"
   type    = string
 }
