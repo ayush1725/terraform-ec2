6a227e0c850ff61b890b4aa11a089ad4c5b545d8
Ayush <ayushdwibedy123@gmail.com>
2025-06-14 23:11:10 +0530

    Added backup by DynamoDB and S3 bucket

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,15 @@
-output "ec2_public_ip" {
-  value = aws_instance.my_instance[*].public_ip
+output "ec2_public_ips" {
+  value = [for instance in aws_instance.my_instance : instance.public_ip]
 }
 
 output "ec2_public_dns" {
-  value = aws_instance.my_instance[*].public_dns
+  value = [for instance in aws_instance.my_instance : instance.public_dns]
 }
-output "ec2_private_ip" {
-  value = aws_instance.my_instance[*].private_ip
+
+output "ec2_private_ips" {
+  value = [for instance in aws_instance.my_instance : instance.private_ip]
 }
-output "ec2_instance_id" {
-  value = aws_instance.my_instance[*].id
+
+output "ec2_instance_ids" {
+  value = [for instance in aws_instance.my_instance : instance.id]
 }
--- terraform.tf
+++ terraform.tf
@@ -5,4 +5,11 @@ terraform {
       version = "6.0.0-beta3"
     }
   }
+
+  backend "s3" {
+    bucket         = "tws-junoon-state-bucket-ayush"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "tws-junoon-state-table"
+  }
 }
